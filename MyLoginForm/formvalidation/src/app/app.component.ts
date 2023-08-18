f8c468ef218835028b0b8a5b3e80cb57fc377364
unknown <kamatabhishek14@gmail.com>
2023-08-18 19:51:44 +0530

    loginform project

---
+++ MyLoginForm/formvalidation/src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { FormGroup,Validators,FormControl} from '@angular/forms';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'fromvalidation';
+
+  loginForm = new FormGroup({
+    user: new FormControl('',[Validators.required,Validators.email]),
+    password: new FormControl('',[Validators.required,Validators.minLength(5)])
+  })
+
+  loginUser(){
+    console.warn(this.loginForm.value)
+    alert("added user successfully")
+  }
+
+  get user(){
+    return this.loginForm.get("user")
+  }
+
+  get password(){
+    return this.loginForm.get("password")
+  }
+}
